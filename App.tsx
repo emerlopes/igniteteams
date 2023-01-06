d593a8902b4ca0b395e0f8898b501d4e5f92a661
Emerson <lopes.ds@live.com>
2023-01-06 00:11:16 -0300

    implementation theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
+import theme from '@theme/index'
+import { Loading } from "@components/Loading";
+
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
 
