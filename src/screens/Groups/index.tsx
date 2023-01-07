03f0f8dfd75eaa87ef0624324f862fdcc4b199e6
Emerson <lopes.ds@live.com>
2023-01-06 23:17:19 -0300

    new component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container, Title } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
+      <GroupCard title={'Galera do Paineiras'} />
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,7 +4,10 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
+    padding: 10% 5%;
+    
+    
 `;
 
 export const Title = styled.Text`
