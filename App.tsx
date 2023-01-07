6170680f470ba96b901f2eb532cd1495208c9ade
Emerson <lopes.ds@live.com>
2023-01-06 23:18:08 -0300

    insert statusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
@@ -11,6 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar  />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
