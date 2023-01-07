88ceb0074e7e9edc54bc0af5ef672b6450132af7
Emerson <lopes.ds@live.com>
2023-01-06 23:43:39 -0300

    implementing the FlatList component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,29 @@
+import { FlatList } from 'react-native';
+
+import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container, Title } from './styles';
 
+
 export function Groups() {
+
+  const [groups, setGroups] = useState(['Galera do Sábado', 'Galera de Segunda', 'Galera do Itaú']);
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
-      <GroupCard title={'Galera do Paineiras'} />
+      <FlatList style={{ width: '100%' }}
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
