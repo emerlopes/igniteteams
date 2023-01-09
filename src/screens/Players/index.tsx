fb8e3eb0a1c37692d7f7ed898d72ec20625eab16
Emerson <lopes.ds@live.com>
2023-01-08 23:30:25 -0300

    Creating Form to InputText and ButtonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,17 +3,17 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 export function Players() {
     return (
         <Container>
             <Header showBackButton />
             <Highlight title="Nome da turma" subtitle="Adicione a galera e separe os times" />
-
-            <ButtonIcon icon="home"/>
-
-            <Input placeholder="Nome do participante" />
+            <Form>
+                <Input placeholder="Nome do participante" autoCorrect={false} />
+                <ButtonIcon icon="add" />
+            </Form>
             <Button title="Remover turma" type="SECUNDARY" style={{ marginTop: 20 }} />
         </Container>
     );
