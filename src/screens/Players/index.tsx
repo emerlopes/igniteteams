89a71f74291e84e42b2f5d360d916fb0c7e3a18a
Emerson <lopes.ds@live.com>
2023-01-09 14:40:17 -0300

    Update team name

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -17,7 +17,7 @@ export function Players() {
     return (
         <Container>
             <Header showBackButton />
-            <Highlight title="Nome da turma" subtitle="Adicione a galera e separe os times" />
+            <Highlight title="Paineiras" subtitle="Adicione a galera e separe os times" />
             <Form>
                 <Input placeholder="Nome do participante" autoCorrect={false} />
                 <ButtonIcon icon="add" />
