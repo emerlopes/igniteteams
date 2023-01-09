d737b2c6d6f1b310b67e14f9b4ffafa04897041a
Emerson <lopes.ds@live.com>
2023-01-09 00:25:17 -0300

    Implementing Screen Header Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
+    const [team, setTeam] = useState("Time A");
+    const [players, setPlayers] = useState(['Emerson', 'John', 'Mary']);
+
+
     return (
         <Container>
             <Header showBackButton />
@@ -15,7 +21,24 @@ export function Players() {
                 <Input placeholder="Nome do participante" autoCorrect={false} />
                 <ButtonIcon icon="add" />
             </Form>
-            <Filter title="Time A" />
+
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    horizontal={true}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+
+                        />
+                    )}
+                />
+                <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+            </HeaderList>
+
             <Button title="Remover turma" type="SECUNDARY" style={{ marginTop: 20 }} />
         </Container>
     );
