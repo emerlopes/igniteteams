c7263914912b6b0a625b4c0861668e6d4bbc2ecc
Emerson <lopes.ds@live.com>
2023-01-10 23:58:51 -0300

    Added error handling

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,29 +1,35 @@
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { setStorageGroup } from '@storage/group/setStorageGroup';
+import { AppError } from '@utils/AppError';
 
 import { Conainer, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { Alert } from 'react-native';
+
 
 
 export function NewGroup() {
     const [group, setGroup] = useState('');
-
-
     const navigation = useNavigation();
 
     async function actionCreateNewGroup() {
         try {
-            await setStorageGroup(group);
+            await setStorageGroup(group.trim());
             navigation.navigate('players', { group });
         } catch (e) {
-            console.log(e)
+            if (e instanceof AppError) {
+                Alert.alert("Nova turma", e.message);
+            } else {
+                Alert.alert("Nova turma", "Não foi possível criar um novo grupo");
+                console.log(e)
+            }
+
         }
     }
-
 
     return (
         <Conainer>
@@ -33,6 +39,8 @@ export function NewGroup() {
                 <Highlight title='Nova turma' subtitle='Crie uma turma para adicionar pessoas' />
                 <Input placeholder='Nome da turma'
                     onChangeText={setGroup}
+                    keyboardType={'name-phone-pad'}
+                    maxLength={29}
                 />
                 <Button title='Criar' style={{ marginTop: 20 }}
                     onPress={actionCreateNewGroup}
--- src/storage/group/setStorageGroup.ts
+++ src/storage/group/setStorageGroup.ts
@@ -1,17 +1,26 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppError } from '@utils/AppError';
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { getStorageGroups } from './getStorageGroup';
 
 export async function setStorageGroup(newGroup: string) {
     try {
+
+        if (!newGroup) {
+            throw new AppError("Preencha o nome da turma");
+        }
+
         const storedGroups = await getStorageGroups();
+        const groupAlreadyExists = storedGroups.includes(newGroup);
+
+        if (groupAlreadyExists) {
+            throw new AppError("Já existe um grupo cadastrado com esse nome!");
+        }
 
         const storage = JSON.stringify([...storedGroups, newGroup]);
-
         await AsyncStorage.setItem(GROUP_COLLECTION, storage);
 
-    } catch (e) {
-        throw e;
-
+    } catch (error) {
+        throw error;
     }
 }
