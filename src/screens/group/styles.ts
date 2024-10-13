264efc34aac5d85b3107af1c71b4655df50dd52c
Emerson <lopes.ds@live.com>
2024-10-13 20:09:24 -0300

    chore: criando estilizacao de componentes

---
+++ src/screens/group/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+    flex: 1;
+    flex-direction: row;
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    color: #FFF;
+    font-size: 32px;
+`;
+
+export const GroupName = styled.TextInput`
+    flex: 1;
+    background-color: aliceblue;
+    height: 52px;
+    font-size: 24px;
+    padding: 8px;
+`;
+
