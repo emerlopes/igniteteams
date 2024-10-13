5a3fdc7aba9859c62a1668dbdc81802eca4b622f
Emerson <lopes.ds@live.com>
2024-10-13 20:50:08 -0300

    chore: carregando fonts na aplicacao.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Group } from '@/screens/group';
 import theme from '@/theme';
+import { ActivityIndicator, ActivityIndicatorBase } from 'react-native';
 
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Group />
+        {fontsLoaded ? <Group /> : <ActivityIndicator />}
       </ThemeProvider>
     </>
   );
