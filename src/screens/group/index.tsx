47d7d5a48dddf3b046bad00e720594656e44d2e3
Emerson <lopes.ds@live.com>
2024-10-13 20:49:38 -0300

    chore: removendo componentes do Container

--- src/screens/group/index.tsx
+++ src/screens/group/index.tsx
@@ -1,10 +1,8 @@
-import { Container, Title, GroupName } from '@/screens/group/styles';
+import { Container } from '@/screens/group/styles';
 
 export function Group() {
   return (
     <Container>
-      <Title>Group</Title>
-      <GroupName></GroupName>
     </Container>
   );
 }
