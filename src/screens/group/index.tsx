11d55597ef28450f916e7d142e51ce31c4803c54
emerlopes <lopes.ds@live.com>
2024-10-16 08:43:04 -0300

    chore: implementando highlight

--- src/screens/group/index.tsx
+++ src/screens/group/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/components/header';
+import { Highlight } from '@/components/highlight';
 import { Container } from '@/screens/group/styles';
 
 export function Group() {
   return (
     <Container>
       <Header />
+      <Highlight title="Emerson" subTitle="Lopes"/>
     </Container>
   );
 }
