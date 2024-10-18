cd13945f9d129ceac83db96180605a4a733e1bad
emerlopes <lopes.ds@live.com>
2024-10-17 23:40:39 -0300

    chore: implementando listagem de grupos

---
+++ src/helper/groups.ts
@@ -0,0 +1,12 @@
+export type Group = {
+    id: string
+    title: string
+}
+
+export const mockGroups: Group[] = [
+    { id: "1", title: "Sociedade Esportiva Palmeiras " },
+    { id: "2", title: "Real Madrid" },
+    { id: "3", title: "Barcelona FC" },
+    { id: "4", title: "Mancheter City" },
+    { id: "5", title: "Mancheter United" }
+]
--- src/screens/group/index.tsx
+++ src/screens/group/index.tsx
@@ -1,15 +1,30 @@
 import { GroupCard } from '@/components/GroupCard';
 import { Header } from '@/components/header';
 import { Highlight } from '@/components/highlight';
+import { Group, mockGroups } from '@/helper/groups';
 import { Container } from '@/screens/group/styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
-export function Group() {
+
+type Props = {
+  groups: Group[]
+}
+
+export function Groups() {
+
+  const [groups, setGroups] = useState<Group[]>(mockGroups)
+
   return (
     <Container>
       <Header />
-      <Highlight title="Turma" subTitle="jogue com a sua turma"/>
+      <Highlight title="Turma" subTitle="jogue com a sua turma" />
 
-      <GroupCard title="Paineiras FC"/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <GroupCard title={item.title} />}
+      />
     </Container>
   );
 }
