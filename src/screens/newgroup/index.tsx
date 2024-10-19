30e161c64ee49d4c2515c7c825e79f9fc15a56d1
emerlopes <lopes.ds@live.com>
2024-10-18 23:19:47 -0300

    chore: criando input

--- src/screens/newgroup/index.tsx
+++ src/screens/newgroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "@/screens/newgroup/styles";
 import { Header } from "@/components/header";
 import { Button } from "@/components/button";
 import { Highlight } from "@/components/highlight";
+import { Input } from "@/components/input";
 
 export function NewGroup() {
     return (
@@ -16,6 +17,7 @@ export function NewGroup() {
                     title="Nova turma"
                     subTitle="crie a turma para adicionar as pessoas"
                 />
+                <Input />
 
                 <Button
                     title="Criar"
