ab50e03ccd708ef27dd84fbefe8bfa25ca201434
emerlopes <lopes.ds@live.com>
2024-10-18 23:24:40 -0300

    chore: implementando placeholder

--- src/screens/newgroup/index.tsx
+++ src/screens/newgroup/index.tsx
@@ -17,7 +17,7 @@ export function NewGroup() {
                     title="Nova turma"
                     subTitle="crie a turma para adicionar as pessoas"
                 />
-                <Input />
+                <Input placeholder="Nome da turma"/>
 
                 <Button
                     title="Criar"
